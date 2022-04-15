2c92eb18f050e55861af0c46c8555affef177133
Josiah Douglass <josiahdouglass64@gmail.com>
2022-04-15 17:59:45 -0400

    added employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,14 @@
+const Employee = require('../lib/Employee.js');
+
+test('Creates new employee', () => {
+    const employee = new Employee('jeff', 'jeff@mail', 3);
+
+    expect(employee.name).toBe('jeff');
+    expect(employee.email).toBe('jeff@mail');
+    expect(employee.id).toEqual(expect.any(Number));
+
+    expect(employee.getName()).toEqual({"name": "jeff"});
+    expect(employee.getId()).toEqual({"id": 3});
+    expect(employee.getEmail()).toEqual({"email": "jeff@mail"});
+    expect(employee.getRole()).toEqual({"role": "Employee"});
+});
